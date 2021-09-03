a207b6d7decb0286b86a98c8aa1a4a3dfba93ee0
Dawid Zawadzki <lamazlo21@gmail.com>
2021-09-04 00:07:50 +0200

    Create and apply global style

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import GlobalStyle from '../src/GlobalStyle'
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +9,14 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => {
+    return (
+      <>
+        <GlobalStyle/>
+        <Story/>
+      </>
+    )
+  }
+]
---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+    * {
+        padding: 0;
+        margin: 0;
+    }
+
+    html {
+        --black: rgb(0, 0, 0);
+        --yellow-main: rgb(249, 212, 35);
+    }
+`
