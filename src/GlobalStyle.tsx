7fcc28af04ab2537865f9825f9098a6a1d304ac8
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-06 15:29:49 +0200

    Add new global styles

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -10,6 +10,23 @@ export default createGlobalStyle`
         --white: rgb( 255, 255, 255);
         --black: rgb(0, 0, 0);
         --yellow-main: rgb(249, 212, 35);
+        --orange-main: rgb(248, 54, 0);
         --border-width: 2px;
+        --nav-mobile-height: 80px;
+        --footer-mobile-height: 80px;
+        --gradient-main: linear-gradient(to right, var(--yellow-main), var(--orange-main));
+    }
+
+    ul {
+        list-style-type: none;
+    }
+
+    button {
+        border: none;
+        background: none;
+
+        &:hover {
+            cursor: pointer;
+        }
     }
 `
