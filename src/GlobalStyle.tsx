b2d17e23b55c973531603de9ea6e70fb200ea8bc
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-10 00:02:06 +0200

    Add Roboto font

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -7,14 +7,18 @@ export default createGlobalStyle`
     }
 
     html {
+        font-family: 'Roboto', sans-serif;
+        font-size: 62.5%;
         --white: rgb( 255, 255, 255);
         --black: rgb(0, 0, 0);
         --yellow-main: rgb(249, 212, 35);
         --orange-main: rgb(248, 54, 0);
+        --shadow-color: rgba(0, 0, 41, 0.15);
         --border-width: 2px;
         --nav-mobile-height: 80px;
         --footer-mobile-height: 80px;
         --gradient-main: linear-gradient(to right, var(--yellow-main), var(--orange-main));
+        --link-font-size: 1.6rem;
     }
 
     ul {
