249cd729df6f7583c0fc423609ed55ab5cc7e8e0
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-10 00:03:14 +0200

    Update Link component

--- src/components/link/Link.stories.tsx
+++ src/components/link/Link.stories.tsx
@@ -1,6 +1,10 @@
 import { StyledComponent } from 'styled-components'
-import { LinkDefault, LinkButton, Props } from './Link'
+import { LinkDefault, LinkButton, LinkDownload, LinkDocument, Props, PropsDownload, PropsDocument } from './Link'
 import { BrowserRouter as Router } from 'react-router-dom'
+
+// File
+// import Rulebook from '/public/assets/documents/Regulamin-organizacji-imprez.pdf'
+// import Agreement from '/public/assets/documents/Zgoda-rodzica-opiekuna.pdf'
 
 export default {
     title: 'Components/Link',
@@ -64,5 +68,53 @@ export const Button = (args: Props) => {
 Button.args = {
     path: '/example',
     text: 'Kup bilet',
+    color: 'black'
+}
+
+export const Download = (args: PropsDownload) => {
+    return (
+        <div
+            style={{
+                height: '100%',
+                backgroundColor: 'rgb(255, 255, 255)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
+        >
+            <LinkDownload
+                {...args}
+            />
+        </div>
+    )
+}
+
+Download.args = {
+    filename: 'xd',
+    text: 'Regulamin blabla',
+    color: 'black'
+}
+
+export const Document = (args: PropsDocument) => {
+    return (
+        <div
+            style={{
+                height: '100%',
+                backgroundColor: 'rgb(255, 255, 255)',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
+        >
+            <LinkDocument
+                {...args}
+            />
+        </div>
+    )
+}
+
+Document.args = {
+    document: 'xd',
+    text: 'Regulamin blabla',
     color: 'black'
 }
--- src/components/link/Link.tsx
+++ src/components/link/Link.tsx
@@ -1,5 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { NavLink } from 'react-router-dom'
+
+// Components
+import Icon from '../icon/Icon'
 
 type LinkColor = 'white' | 'black'
 
@@ -14,12 +17,19 @@ export interface Props {
     color: LinkColor
 }
 
+export interface PropsDownload extends Omit<Props, 'path'> {
+    filename: string
+}
+
+export interface PropsDocument extends Omit<Props, 'path'> {
+    document: string
+}
+
 type LinkProps = Pick<Props, 'color'>
 
-const Default = styled(NavLink)<LinkProps>`
-    white-space: nowrap;
-    display: inline-block;
-    text-decoration: none;
+const BaseStyles = css<LinkProps>`
+    font-size: var(--link-font-size);
+
     color: ${ props => 
         props.color === 'white' ? 'var(--white)' : 'var(--black)'
     };
@@ -29,27 +39,18 @@ const Default = styled(NavLink)<LinkProps>`
     }
 `
 
-const Button = styled(NavLink)`
-    padding: 10px 15px;
+const IconStyles = css`
+    display: grid;
+    gap: 10px;
+`
+
+// Default link
+
+const Default = styled(NavLink)<LinkProps>`
+    ${BaseStyles}
     white-space: nowrap;
     display: inline-block;
     text-decoration: none;
-
-    border: var(--border-width) solid ${ props => 
-        props.color === 'white' ? 'var(--white)' : 'var(--black)'
-    };
-
-    color: ${ props => 
-        props.color === 'white' ? 'var(--white)' : 'var(--black)'
-    };
-
-    &.active, &:hover {
-        color: var(--yellow-main);
-    }
-
-    &.active, &:hover {
-        border-color: var(--yellow-main);
-    }
 `
 
 export const LinkDefault = ({
@@ -67,6 +68,8 @@ export const LinkDefault = ({
     )
 }
 
+// Button link
+
 export const LinkButton = ({
     path,
     text,
@@ -79,5 +82,80 @@ export const LinkButton = ({
         >
             { text }
         </Button>
+    )
+}
+
+const Button = styled(NavLink)`
+    ${BaseStyles}
+    padding: 10px 15px;
+    white-space: nowrap;
+    display: inline-block;
+    text-decoration: none;
+
+    border: var(--border-width) solid ${ props => 
+        props.color === 'white' ? 'var(--white)' : 'var(--black)'
+    };
+
+    &.active, &:hover {
+        border-color: var(--yellow-main);
+    }
+`
+
+// Download link
+
+const Download = styled.a`
+    ${BaseStyles}
+    ${IconStyles}
+    align-items: center;
+    grid-template-rows: 45px;
+    grid-template-columns: 45px auto;
+`
+
+export const LinkDownload = ({
+    text,
+    color,
+    filename
+}: PropsDownload) => {
+    return (
+        <Download
+            color={color}
+            href={filename}
+            download={text}
+        >
+            <Icon
+                image={'icon-download'}
+            />
+            { text }
+        </Download>
+    )
+}
+
+// Document link
+
+const Document = styled.a`
+    ${BaseStyles}
+    ${IconStyles}
+    text-align: center;
+    justify-items: center;
+    grid-template-rows: 45px auto;
+`
+
+export const LinkDocument = ({
+    text,
+    color,
+    document
+}: PropsDocument) => {
+    return (
+        <Document
+            color={color}
+            href={document}
+            target={'_blank'}
+            rel={'noreferrer'}
+        >
+            <Icon
+                image={'icon-document'}
+            />
+            { text }
+        </Document>
     )
 }
