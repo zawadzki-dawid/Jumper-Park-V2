f0da9ac7614c7d8d69883f5c4e081cc26546ecf2
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-19 23:27:15 +0200

    Add main Router component

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import GlobalStyle from '../src/GlobalStyle'
+import App from './App'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import GlobalStyle from './GlobalStyle'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle/>
-    <App />
+      <Router>
+        <App />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
