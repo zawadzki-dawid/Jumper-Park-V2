557cd4fa207b0b7fc9eea0b35ca7d6eb5c0b14fc
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-30 00:39:30 +0200

    Change Heading component to match wireframes

--- src/components/heading/Heading.tsx
+++ src/components/heading/Heading.tsx
@@ -5,26 +5,11 @@ export interface Props {
 }
 
 const Heading = styled.header`
-    display: flex;
-    align-items: center;
-
     h2 {
-        padding: 6px 20px;
+        padding: 8px 15%;
         width: fit-content;
-        border: var(--border-width) solid var(--yellow-main);
-
-    }
-
-    div {
-        width: fit-content;
-    }
-
-    &:before, &:after {
-        width: 10%;
-        content: '';
-        display: block;
-        height: var(--border-width);
-        background-color: var(--yellow-main);
+        font-size: var(--default-font-size);
+        border-bottom: var(--border-width) solid var(--yellow-main);
     }
 `
 
@@ -33,11 +18,9 @@ export default ({
 }: Props) => {
     return (
         <Heading>
-            <div>
                 <h2>
                     { text }
                 </h2>
-            </div>
         </Heading>
     )
 }
