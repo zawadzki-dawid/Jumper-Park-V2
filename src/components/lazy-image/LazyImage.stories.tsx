e1a3cdb4ee46029817ac0353f5dd6ef5910e6159
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-30 00:42:23 +0200

    Create LazyImage component

---
+++ src/components/lazy-image/LazyImage.stories.tsx
@@ -0,0 +1,55 @@
+import { StyledComponent } from 'styled-components';
+import LazyImage, { Props } from './LazyImage';
+
+export default {
+    title: 'Components/LazyImage',
+    args: {
+        alt: 'PFR',
+        image: 'https://firebasestorage.googleapis.com/v0/b/jumper-park.appspot.com/o/flamelink%2Fmedia%2Fpfr.svg?alt=media&token=1309a8ab-3c90-4781-8b10-0dc12004e5c0'
+    },
+    decorators: [
+        (Story: StyledComponent<'div', any>) => (
+            <div
+                style={{
+                    display: 'flex',
+                    height: '2000px',
+                    marginTop: '20px',
+                    justifyContent: 'center'
+                }}
+            >
+                <Story/>
+            </div>
+        )
+    ]
+}
+
+export const VisibleOnRender = (args: Props) => {
+    return (
+        <div
+            style={{
+                width: '300px',
+                height: '300px'
+            }}
+        >
+            <LazyImage
+                {...args}
+            />
+        </div>
+    )
+}
+
+export const VisibleOnScroll = (args: Props) => {
+    return (
+        <div
+            style={{
+                width: '300px',
+                height: '300px',
+                marginTop: '1500px'
+            }}
+        >
+            <LazyImage
+                {...args}
+            />
+        </div>
+    )
+}
---
+++ src/components/lazy-image/LazyImage.tsx
@@ -0,0 +1,114 @@
+import styled from 'styled-components'
+import { useEffect, useRef, useState } from 'react'
+
+export interface Props {
+    alt: string
+    image: string
+}
+
+const Wrapper = styled.div`
+    width: 100%;
+    position: relative;
+    height: fit-content;
+`
+
+const Image = styled.img`
+    width: 100%;
+`
+
+const Loader = styled.div`
+    top: 0;
+    height: 100%;
+    position: absolute;
+    background: var(--image-loader-gradient);
+    background-size: 400% 400%;
+    animation: loading 1.5s ease infinite;
+
+    @keyframes loading {
+        0% {
+            background-position: 31% 0%;
+        }
+
+        50% {
+            background-position: 70% 100%;
+        }
+
+        100% {
+            background-position: 31% 0%;
+        }
+    }
+`
+
+export default ({
+    alt,
+    image
+}: Props) => {
+    // State
+    const [isLoading, setIsLoading] = useState<boolean>(true)
+    const [isVisible, setIsVisible] = useState<boolean>(false)
+
+    // Ref
+    const wrapperRef = useRef<HTMLDivElement | null>(null)
+
+    // Effect
+    useEffect(() => {
+        if (!wrapperRef.current) {
+            return
+        }
+
+        if (isVisibleOnRender()) {
+            setIsVisible(true)
+            return
+        }
+
+        const observer = new IntersectionObserver(onIntersection, {
+            threshold: 0.01,
+            rootMargin: '30px 30px'
+        })
+        observer.observe(wrapperRef.current)
+
+        return () => {
+            observer.disconnect()
+        }
+    }, [])
+
+    // Method
+    const isVisibleOnRender = () => {
+        if (!wrapperRef.current) {
+            return
+        }
+        return window.innerHeight - wrapperRef.current.offsetTop > 0
+    }
+
+    const onIntersection = (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                setIsVisible(true)
+                observer.disconnect()
+            }
+        })
+    }
+
+    const onLoaded = () => {
+        setIsLoading(false)
+    }
+
+    return (
+        <Wrapper
+            ref={wrapperRef}
+        >
+            {
+                isVisible && (
+                    <Image
+                        alt={alt}
+                        src={image}
+                        onLoad={onLoaded}
+                    />
+                )
+            }
+            {
+                isVisible && isLoading && <Loader/>
+            }
+        </Wrapper>
+    )
+}
