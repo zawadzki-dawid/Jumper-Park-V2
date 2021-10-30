d0e2da10faea01e3eb7161e4ac4d6438b2de8eed
Dawid Zawadzki <lamazlo21@gmail.com>
2021-10-30 21:09:48 +0200

    Add Download icon reference to Icon component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -3,11 +3,13 @@ import styled from 'styled-components'
 // Assets
 import Arrow from '../../assets/icons/icon-arrow.svg'
 import Document from '../../assets/icons/icon-document.svg'
+import Download from '../../assets/icons/icon-download.svg'
 import Hamburger from '../../assets/icons/icon-hamburger.svg'
 
 const icon = {
     arrow: Arrow,
     document: Document,
+    download: Download,
     hamburger: Hamburger
 }
 
