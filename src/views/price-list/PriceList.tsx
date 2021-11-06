b74ea8ddf6c294fb50d9d8b9b895a3c14eb8f3af
Dawid Zawadzki <lamazlo21@gmail.com>
2021-11-06 16:09:14 +0100

    Add PriceList view component

--- src/views/price-list/PriceList.tsx
+++ src/views/price-list/PriceList.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from 'react'
 import Baner from '../../components/baner/Baner'
+import { useFetchContents } from '../../utils/hooks/fetch'
 
 export default () => {
+    // State
+    const { data, error, loading } = useFetchContents('cennik')
+
+    useEffect(() => {
+        console.log(data)
+    }, [data])
+
     return (
         <>
             <Baner 
