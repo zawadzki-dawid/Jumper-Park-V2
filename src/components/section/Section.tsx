7a2766d1a56285b2e7503ebd64446627d94235b0
lamazlo21@gmail.com <lamazlo21@gmail.com>
2021-11-11 18:52:41 +0100

    Refactor Section component

--- src/components/section/Section.tsx
+++ src/components/section/Section.tsx
@@ -5,39 +5,56 @@ import styled from 'styled-components'
 import Heading from '../heading/Heading'
 
 interface Props {
-    text: string
+    text?: string
     children: ReactElement
 }
 
 const Section = styled.section`
-    margin-top: 30px;
+    display: flex;
+    height: fit-content;
+    margin: 30px 20px;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
     font-size: var(--heading-font-size);
 
     > div {
-        display: flex;
-        margin-top: 20px;
-        align-items: center;
-        flex-direction: column;
+        width: 100%;
+        max-width: 1280px;
     }
 
-    @media only screen and (min-width: 1000px) {
-        margin-top: 50px;
+    > div:not(:last-of-type) {
+        margin-bottom: 15px;
+    }
 
-        > div {
-            margin-top: 35px;
+    @media only screen and (min-width: 900px) {
+        margin: 60px 60px;
+
+        > div:not(:last-of-type) {
+            margin-bottom: 20px;
         }
+    }
+
+    &:not(:last-child) {
+        margin-bottom: 0px;
     }
 `
 
 export default ({
-    text,
+    text = '',
     children
 }: Props) => {
     return (
         <Section>
-            <Heading
-                text={text}
-            />
+            {
+                text.length !== 0 && (
+                    <div>
+                        <Heading
+                            text={text}
+                        />
+                    </div>
+                )
+            }
             <div>
                 { children }
             </div>
