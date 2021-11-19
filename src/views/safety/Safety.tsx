cfdb5d858ed513f61826ed5f2013d87af572fabe
lamazlo21@gmail.com <lamazlo21@gmail.com>
2021-11-19 19:07:30 +0100

    Add form to Safety view

--- src/views/safety/Safety.tsx
+++ src/views/safety/Safety.tsx
@@ -1,14 +1,29 @@
 import styled from 'styled-components'
+import { useContext, useEffect } from 'react'
+import { LoaderContext } from '../../components/loader/Loader'
+
+// Components
+import FormMain from '../../components/form/form-main/FormMain'
 
 // Sections
 import Baner from '../../components/baner/Baner'
 import DownloadSection from './download-section/DownloadSection'
+
+// Main
 
 const Wrapper = styled.div`
     margin: 25px 0;
 `
 
 export default () => {
+    // Context
+    const { setEntered } = useContext(LoaderContext)
+
+    // Effect
+    useEffect(() => {
+        setEntered(false)
+    }, [])
+
     return (
         <>
             <Baner
@@ -16,6 +31,7 @@ export default () => {
             />
             <Wrapper>
                 <DownloadSection/>
+                <FormMain/>
             </Wrapper>
         </>
     )
