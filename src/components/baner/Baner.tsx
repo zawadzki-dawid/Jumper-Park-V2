c37114e19da3d4c114472a63942c19f8b561eb05
lamazlo21@gmail.com <lamazlo21@gmail.com>
2021-11-21 12:35:45 +0100

    Make Baner content not wider than 1280px

--- src/components/baner/Baner.tsx
+++ src/components/baner/Baner.tsx
@@ -5,11 +5,24 @@ export interface Props {
 }
 
 const Baner = styled.header`
+    height: 70px;
     display: flex;
-    align-items: center;
     padding-left: 20px;
-    height: 80px;
+    align-items: center;
     background: var(--gradient-main);
+
+    h1 {
+        width: 100%;
+        margin: auto;
+        padding: 0 20px;
+        font-weight: 700;
+        font-size: 2.5rem;
+        max-width: 1280px;
+
+        @media only screen and (min-width: 1000px) {
+            padding: 0 60px;
+        }
+    }
 `
 
 export default ({
