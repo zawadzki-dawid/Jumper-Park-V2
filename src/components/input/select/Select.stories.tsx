0bf05f86e9be1f6d48abc65129e6ffcdefd2a324
lamazlo21@gmail.com <lamazlo21@gmail.com>
2021-12-08 01:56:52 +0100

    Create Select component

--- src/components/input/select/Select.stories.tsx
+++ src/components/input/select/Select.stories.tsx
@@ -1,7 +1,64 @@
+import * as Yup from 'yup'
+
+import Select from './Select'
+import { Formik } from 'formik';
+import { StyledComponent } from 'styled-components';
+
 export default {
-    title: 'Components/Select'
+    title: 'Components/Form/Organisms/Select',
+    decorators: [
+        (Story: StyledComponent<'div', any>) => (
+            <div
+                style={{
+                    height: '90vh',
+                    display: 'flex',
+                    margin: '0 50px',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}
+            >
+                <Formik
+                    validationSchema={
+                        Yup.object({
+                            select: Yup.string().required()
+                        })
+                    }
+                    initialValues={{
+                        select: '',
+                    }}
+                    onSubmit={
+                        () => {}
+                    }
+                >
+                    <Story/>
+                </Formik>
+            </div>
+        )
+    ]
 }
 
-export const XD = () => {
-    return 6
+export const Default = () => {
+    return (
+        <div
+            style={{
+                width: '100%'
+            }}
+        >
+            <Select/>
+        </div>
+    )
+}
+
+Default.args = {
+    id: 'select',
+    name: 'select',
+    label: 'Imię i nazwisko',
+    options: [
+        'Urodziny',
+        'Zakup biletu',
+        'Pomoc',
+        'Zajęcia',
+        'Pytanie',
+        'Atrakcje'
+    ]
 }
